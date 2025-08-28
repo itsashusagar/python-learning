81a6f31b54950209d2840cba0440eb2020543016
ashu sagar <itsashusagar@gmail.com>
2025-08-28 11:16:54 +0530

    commit

---
+++ statistics/first.py
@@ -0,0 +1,8 @@
+import numpy as np
+from statistics import mean, median, mode
+
+data = [2, 4, 4, 4, 5, 7, 9]
+
+print("Mean:", mean(data))
+print("Median:", median(data))
+print("Mode:", mode(data))
