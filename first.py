4ab0a8c08816c9af12a8dc179e869d171cb80c8b
ashu sagar <itsashusagar@gmail.com>
2025-08-28 10:57:55 +0530

    commit

---
+++ first.py
@@ -0,0 +1,5 @@
+# Vector ko Python list se represent karte hain
+v1 = [3, 2]   # Matlab 3 step right aur 2 step up
+v2 = [1, 4]   # Matlab 1 step right aur 4 step up
+print("Vector 1:", v1)
+print("Vector 2:", v2)
